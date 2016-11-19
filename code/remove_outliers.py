c6bab82069577d19f26cb759c75988dd6627e74c
dokutagero <kendixd@gmail.com>
2016-11-19 13:47:59 +0100

    Changed value for removing outliers

--- code/remove_outliers.py
+++ code/remove_outliers.py
@@ -5,17 +5,17 @@ new_list = []
 #maxz =  
 #miny = 0
 
-with open('odm_mesh2.xyz', 'rb') as csvfile:
+with open('../dataset/odm_mesh_small.xyz', 'rb') as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=' ')
 
 	for row in csvreader:
 		# print row
-		if float(row[-2]) > -200:
-			new_list.append(row[:-1])
+		if float(row[-1]) < -100:
+			new_list.append(row)
 
 # print new_list
 
 
-with open('odm_mesh_prueba.xyz', 'wb') as csvfile:
+with open('../dataset/odm_mesh_small2.xyz', 'wb') as csvfile:
 	csvwriter = csv.writer(csvfile, delimiter=' ')
 	csvwriter.writerows(new_list)
